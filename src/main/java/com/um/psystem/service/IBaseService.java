ba965412fc32731cd35a0592e72d5ae732265275
hizzj <15768653615@163.com>
2020-05-28 16:13:31 +0800

    刷新权限缓存

--- src/main/java/com/um/psystem/service/IBaseService.java
+++
@@ -1,9 +0,0 @@
-package com.um.psystem.service;
-
-import com.baomidou.mybatisplus.service.IService;
-
-/**
- * Created by zzj on 2020-05-19.
- */
-public interface IBaseService<T> extends IService<T> {
-}
