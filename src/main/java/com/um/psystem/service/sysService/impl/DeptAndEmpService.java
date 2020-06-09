5db1c250753871d8080032f508fc245a71b1f33d
hizzj <15768653615@163.com>
2020-06-09 11:45:32 +0800

    新增一些工具类

---
+++ src/main/java/com/um/psystem/service/sysService/impl/DeptAndEmpService.java
@@ -0,0 +1,26 @@
+package com.um.psystem.service.sysService.impl;
+
+import com.um.psystem.entity.sysEntity.DeptEntity;
+import com.um.psystem.mapper.platform.mtManageMapper.DeptAndEmpMapper;
+import com.um.psystem.service.sysService.IDeptAndEmpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: zhenjin.zheng
+ * @Description:
+ * @Date: 2020/6/2
+ */
+@Service
+public class DeptAndEmpService implements IDeptAndEmpService {
+
+    @Autowired
+    DeptAndEmpMapper deptAndEmpMapper;
+    @Override
+    public List<DeptEntity> getDeptList(Map<String, Object> columnMap) {
+        return deptAndEmpMapper.selectByMap(columnMap);
+    }
+}
